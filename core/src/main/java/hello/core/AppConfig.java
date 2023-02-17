2fb737ccf4b38607b50768294ade01175ece591c
inpink <inpink@kakao.com>
2023-02-18 01:44:45 +0900

    AppConfig를 이용한 손쉬운 정책 변경

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -2,6 +2,7 @@ package hello.core;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
@@ -38,6 +39,7 @@ public class AppConfig { //Spring Container 역할을 함.
     }
 
     public DiscountPolicy discountPolicy(){
-        return new FixDiscountPolicy();
+        //return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 }
